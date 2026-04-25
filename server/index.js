58b55182acd220f3c56096268a63439ae227d456
Priyesh Dwivedi <priyesh88088@gmail.com>
2026-04-26 01:28:08 +0530

    Update CORS configuration to allow all origins

--- server/index.js
+++ server/index.js
@@ -23,12 +23,10 @@ dbConnect();
 app.use(express.json());
 app.use(cookieParser());
 
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    credentials: true,
-  })
-);
+app.use(cors({
+  origin: "*",
+  credentials: true
+}));
 
 app.use(
   fileUpload({
